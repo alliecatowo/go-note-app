0033a08017436bffaa1cbb5444bd867929eba6ca
Allison Emily Coleman <allison@parseceducation.com>
2024-04-16 10:28:34 -0700

    feat: embedd user into admin struct

--- structs.go
+++ structs.go
@@ -11,16 +11,26 @@ func main() {
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	appUser, err := user.New(userFirstName, userLastName, userBirthdate)
+	adminEmail := getUserData("Please enter your email: ")
+	adminPassword := getUserData("Please enter your password: ")
 
+	appUser, err := user.New(userFirstName, userLastName, userBirthdate)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
+	adminUser, err := user.NewAdmin(adminEmail, adminPassword)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	appUser.OutputUserDetails()
 	appUser.ClearUserNames()
 	appUser.OutputUserDetails()
+
+	adminUser.OutputUserDetails()
+	adminUser.ClearUserNames()
+	adminUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
--- user/user.go
+++ user/user.go
@@ -13,6 +13,28 @@ type User struct {
 	createdAt time.Time
 }
 
+type Admin struct {
+	email    string
+	password string
+	User
+}
+
+func NewAdmin(email, password string) (*Admin, error) {
+	if email == "" || password == "" {
+		return nil, errors.New("email and password are required fields")
+	}
+	return &Admin{
+		email:    email,
+		password: password,
+		User: User{
+			firstName: "ADMIN",
+			lastName:  "ADMIN",
+			birthdate: "---",
+			createdAt: time.Now(),
+		},
+	}, nil
+}
+
 func New(firstName, lastName, birthdate string) (*User, error) {
 	if firstName == "" || lastName == "" || birthdate == "" {
 		return nil, errors.New("first Name, Last Name, and Birthdate are required fields")
