6990c240247f64ee52a698ce0b42b9f6d48bf49e
Allison Emily Coleman <allison@parseceducation.com>
2024-04-16 10:01:58 -0700

    refactor: seperate to packages

--- structs.go
+++ structs.go
@@ -2,55 +2,25 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"errors"
+
+	"example.com/structs/user"
 )
-
-type User struct {
-	firstName string
-	lastName  string
-	birthdate string
-	createdAt time.Time
-}
-
-func newUser(firstName, lastName, birthdate string) (*User, error) {
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("first Name, Last Name, and Birthdate are required fields")
-	}
-	return &User{
-		firstName: firstName,
-		lastName:  lastName,
-		birthdate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	appUser , err:= newUser(userFirstName, userLastName, userBirthdate)
+	appUser, err := user.NewUser(userFirstName, userLastName, userBirthdate)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	appUser.outputUserDetails()
-	appUser.clearUserNames()
-	appUser.outputUserDetails()
-}
-
-func (u User) outputUserDetails() {
-	fmt.Println("First Name: ", u.firstName)
-	fmt.Println("Last Name: ", u.lastName)
-	fmt.Println("Birthdate: ", u.birthdate)
-}
-
-func (u *User) clearUserNames() {
-	u.firstName = ""
-	u.lastName = ""
+	appUser.OutputUserDetails()
+	appUser.ClearUserNames()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
---
+++ user/user.go
@@ -0,0 +1,37 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func NewUser(firstName, lastName, birthdate string) (*User, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("first Name, Last Name, and Birthdate are required fields")
+	}
+	return &User{
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
+
+func (u User) OutputUserDetails() {
+	fmt.Println("First Name: ", u.firstName)
+	fmt.Println("Last Name: ", u.lastName)
+	fmt.Println("Birthdate: ", u.birthdate)
+}
+
+func (u *User) ClearUserNames() {
+	u.firstName = ""
+	u.lastName = ""
+}
